2d21c98496cbe7adda9f9af629421845f12fb45b
Bruno <brunoetecams@gmail.com>
2025-08-21 22:49:02 -0300

    Adição de comentarios no App.js

--- App.js
+++ App.js
@@ -16,16 +16,17 @@ import { salvandoDados, VerHistorico, LimparHistorico } from './components/verHi
 import { styles } from './estilo/style';
 
 const App = () => {
+
   // Estados para armazenar os dados da API e controlar os inputs
   const [data, setData] = useState(null); // Resposta do ViaCEP
   const [loading, setLoading] = useState(false); // Indicador de carregamento
   const [error, setError] = useState(null); // Erro, se houver
   const [cep, setCep] = useState(''); // CEP informado pelo usuário
-  const [uf, setUf] = useState ('');
-  const [localidade, setLocalidade] = useState ('');
-  const [logradouro, setLogradouro] = useState ('');
-  const [historico, setHistorico] = useState([]);
-  const [botao, setBotao] = useState(false); //adicionar estado para o botão
+  const [uf, setUf] = useState (''); // UF informada pelo usuário
+  const [localidade, setLocalidade] = useState (''); // Localidade informada pelo usuário
+  const [logradouro, setLogradouro] = useState (''); // Logradouro informado pelo usuário
+  const [historico, setHistorico] = useState([]); // Histórico informado pelo usuário
+  const [botao, setBotao] = useState(false); // adicionar estado para o botão
 
   // Função para preencher os campos
   const preencherCampos = (item) => {
@@ -38,11 +39,14 @@ const App = () => {
   return (
     <ScrollView style={styles.container}>
       <View style={styles.forms}>
-        <View style={{  alignItems: "center", justifyContent: "center", marginBottom: 20 }}>
-          <Ionicons name="location-sharp" size={25} color="#922B21" style={{ marginRight: 8 }} />
-            <Text style={styles.title}>Consulta de CEP</Text>
-            <View style={styles.titleLine}/>
-        </View>
+      <View style={{  alignItems: "center", justifyContent: "center", marginBottom: 20 }}>
+      <Ionicons name="location-sharp" size={25} color="#922B21" style={{ marginRight: 8 }} />
+
+        <Text style={styles.title}>Consulta de CEP</Text>
+        <View style={styles.titleLine}/>
+
+      </View>
+
         {/* Campo de entrada do CEP */}
         <TextInput
           style={styles.input}
@@ -82,7 +86,8 @@ const App = () => {
           onChangeText={setLogradouro}
         />
 
-<View style={styles.botao}>
+        {/* Botões */}
+        <View style={styles.botao}>
           <HandleSearch
             cep={cep}
             uf={uf}
@@ -92,7 +97,6 @@ const App = () => {
             setData={setData}
             setLoading={setLoading}
             salvandoDados={salvandoDados}
-            onPress={salvandoDados}
           />
           <LimparCampos
               setCep={setCep}
@@ -105,15 +109,18 @@ const App = () => {
             setHistorico={setHistorico}
             setError={setError}
             setBotao={setBotao}
+
           />
 
-              {botao && (
+        {/* O botão aparece e desaparece junto com o histórico */}
+          {botao && (
             <LimparHistorico
             setHistorico={setHistorico}
             setError={setError}
             setBotao={setBotao}
             />
-            )} 
+          )} 
+
 </View>
 
       {/* Exibe mensagem de erro se houver */}
@@ -143,12 +150,13 @@ const App = () => {
         </View>
       )}
 
+      {/* Exibe os dados das pesquisas anteriores (histórico) */}
       {historico.length > 0 && (
   <View style={styles.localContainer}>
-    <Text style={{ fontSize: 20, fontWeight: 'bold', marginBottom: 10 }}>Histórico de Pesquisas:</Text>
+    <Text style={{ fontSize: 15, color:"#922B21", fontWeight: 'bold', marginBottom: 10 }}>Histórico de Pesquisas:</Text>
     {historico.map((item, index) => (
       <View key={index} style={{ marginBottom: 10, padding: 10, borderWidth: 1, borderRadius: 5 }}>
-        <Text onPress={() => preencherCampos(item)}>CEP: {item.cep}</Text>
+<Text onPress={() => preencherCampos(item)}>CEP: {item.cep}</Text>
 <Text onPress={() => preencherCampos(item)}>Logradouro: {item.logradouro}</Text>
 <Text onPress={() => preencherCampos(item)}>Bairro: {item.bairro}</Text>
 <Text onPress={() => preencherCampos(item)}>Cidade: {item.localidade}</Text>
@@ -163,6 +171,5 @@ const App = () => {
     </ScrollView>
   );
 };
-
 
 export default App;
